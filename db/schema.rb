a703e9c28ec894929bd47899f99abe48946c9078
Angeliano <angeliano.sousa@gmail.com>
2022-01-27 12:31:09 -0300

    Adicionado algumas adaptações para o mobile funcionar de forma mais natural, temos problemas com o calendário e com o formulário nested do quadro por enquanto somente

--- db/schema.rb
+++ db/schema.rb
@@ -55,13 +55,6 @@ ActiveRecord::Schema.define(version: 2022_01_25_202423) do
     t.index ["board_id"], name: "index_tasks_on_board_id"
   end
 
-  create_table "user_profiles", force: :cascade do |t|
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_user_profiles_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -76,5 +69,4 @@ ActiveRecord::Schema.define(version: 2022_01_25_202423) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "tasks", "boards"
-  add_foreign_key "user_profiles", "users"
 end
